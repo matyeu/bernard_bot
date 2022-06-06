ac10696e2f92c6ee08518a3bf6ab6b549b19c823
matyeu <matyeu.lts@gmail.com>
2022-06-06 21:22:36 +0200

    Del embed useless

--- src/config.ts
+++ src/config.ts
@@ -3,25 +3,24 @@ export const GITHUB = "https://github.com/matyeu/bernard";
 export const LINK_DISCORD = "https://discord.gg/xNSKAkrPUg"
 
 export const EMBED_GENERAL = "#2f3136";
-export const EMBED_ERREUR = "#E04F5F";
-export const EMBED_VALIDER = "#33BEA6";
+export const EMBED_ERROR = "#E04F5F";
+export const EMBED_SUCESS = "#33BEA6";
 export const EMBED_INFO = "#69A1CC";
-export const EMBED_BD = "#fffefe";
-export const EMBED_RPG = "#ff5757";
 export const EMBED_CLOSE = "#d4d221";
 
-export const FOOTER = "Bernard - Tous droits réservés";
+export const FOOTER = "Bernard - All rights reserved";
 export const FOOTER_MODERATION = "Bernard - Modération";
 export const FOOTER_LOG = "Bernard - Logs";
 export const FOOTER_TICKET = "Bernard - Tickets";
 
 export const EMOJIS = {
+    check: "983324495865589790",
+    diamondBlueDark: "983109519603269742",
     error: "983085316388843610",
     heart: "983100339613995058",
-    verification: "983106823534043186",
-    diamondBlueDark: "983109519603269742",
-    online: "983117613905350676",
     dnd: "983117604493344819",
+    verification: "983106823534043186",
+    online: "983117613905350676",
     afk: "983117582078971985",
     offline: "983117513778950214",
     speaker: "983292101041217556",
