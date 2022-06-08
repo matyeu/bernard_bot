45318cfede7e94624f25711c182e6471f1b9081f
matyeu <matyeu.lts@gmail.com>
2022-06-08 13:04:40 +0200

    Add events Role

---
+++ src/Events/Role/roleCreate.ts
@@ -0,0 +1,21 @@
+import {BernardClient} from "../../Librairie";
+import {MessageEmbed, Role} from "discord.js";
+import {find} from "../../Models/guild";
+import {EMBED_SUCESS, FOOTER_LOG} from "../../config";
+
+export default async function (client: BernardClient, role: Role) {
+
+    let guildConfig: any = await find(role.guild!.id);
+    let server = guildConfig.channels.logs.server;
+
+    const embed = new MessageEmbed()
+        .setColor(EMBED_SUCESS)
+        .setTitle(`Role creatio,`)
+        .addFields(
+            {name: `🎲 Name (ID)`, value: `<@&${role.id}> (${role.id})`, inline: true}
+        )
+        .setTimestamp()
+        .setFooter({text: FOOTER_LOG, iconURL: role.client.user?.displayAvatarURL({dynamic: true})});
+    return client.getChannel(role.guild, server, {embeds: [embed]});
+
+};
---
+++ src/Events/Role/roleUpdate.ts
@@ -0,0 +1,47 @@
+import {BernardClient} from "../../Librairie";
+import {MessageEmbed, Role, Permissions} from "discord.js";
+import {find} from "../../Models/guild";
+import {EMBED_INFO, FOOTER_LOG} from "../../config";
+
+export default async function (client: BernardClient, oldRole: Role, newRole: Role) {
+
+    let guildConfig: any = await find(oldRole.guild!.id);
+    let server = guildConfig.channels.logs.server;
+
+    if (oldRole.rawPosition !== newRole.rawPosition) return;
+
+    let embed = new MessageEmbed()
+        .setColor(EMBED_INFO)
+        .setTitle('Role update')
+        .addFields({name: `🎲 Name (ID)`, value: `<@&${oldRole.id}> (${oldRole.id})`, inline: true})
+        .setTimestamp()
+        .setFooter({text: FOOTER_LOG, iconURL: oldRole.client.user?.displayAvatarURL({dynamic: true})})
+
+    const oldPermissions = new Permissions(oldRole.permissions);
+    let oldPermissionsString = oldPermissions.toArray().join('\n');
+    const newPermissions = new Permissions(newRole.permissions);
+    let newPermissionsString = newPermissions.toArray().join('\n');
+
+    if (oldRole.name !== newRole.name) {
+        embed.addFields(
+            {name: `🎲 Old Name`, value: `\`${oldRole.name}\` (${oldRole.id}`, inline: true},
+            {name: `🎲 New Name`, value: `\`${newRole.name}\` (${newRole.id})`, inline: true})
+        return client.getChannel(oldRole.guild, server, {embeds: [embed]});
+    }
+
+    if (oldPermissionsString !== newPermissionsString) {
+        embed.addFields(
+            {name: `🎲 News Permissions`, value: `\`\`\`${newPermissionsString}\`\`\``, inline: false}
+        )
+        return client.getChannel(oldRole.guild, server, {embeds: [embed]});
+    }
+
+    if (oldRole.color !== newRole.color) {
+        embed.setColor(newRole.color === 0 ? EMBED_INFO : newRole.color)
+        embed.addFields(
+            {name: `🎲 Old Color`, value: `\`#${oldRole.color}\` (${oldRole.id}`, inline: true},
+            {name: `🎲 New Color`, value: `\`#${newRole.color}\` (${newRole.id})`, inline: true})
+        return client.getChannel(oldRole.guild, server, {embeds: [embed]});
+    }
+
+};
