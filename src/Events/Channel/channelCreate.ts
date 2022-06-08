6fd4a18b80ac75ad1db5f194d2f32464b8c82e8b
matyeu <matyeu.lts@gmail.com>
2022-06-08 18:15:45 +0200

    Update emoji

--- src/Events/Channel/channelCreate.ts
+++ src/Events/Channel/channelCreate.ts
@@ -1,18 +1,20 @@
 import {BernardClient} from "../../Librairie";
 import {GuildChannel, MessageEmbed} from "discord.js";
 import {find} from "../../Models/guild";
-import {EMBED_SUCESS, FOOTER_LOG} from "../../config";
+import {EMBED_SUCESS, EMOJIS, FOOTER_LOG} from "../../config";
 
 export default async function (client: BernardClient, channel: GuildChannel) {
 
     let guildConfig: any = await find(channel.guild!.id);
     let server = guildConfig.channels.logs.server;
 
+    let channelEmoji = client.getEmoji(EMOJIS.channel);
+
     const embed = new MessageEmbed()
         .setColor(EMBED_SUCESS)
         .setTitle(`Channel creation`)
         .addFields(
-            {name: `🎙 Name (ID)`, value: `<#${channel.id}> (${channel.id})`, inline: true}
+            {name: `${channelEmoji} Name (ID)`, value: `<#${channel.id}> (${channel.id})`, inline: true}
         )
         .setTimestamp()
         .setFooter({text: FOOTER_LOG, iconURL: channel.client.user?.displayAvatarURL({dynamic: true})});
--- src/Events/Channel/channelUpdate.ts
+++ src/Events/Channel/channelUpdate.ts
@@ -1,7 +1,7 @@
 import {BernardClient} from "../../Librairie";
 import {MessageEmbed, TextChannel} from "discord.js";
 import {find} from "../../Models/guild";
-import {EMBED_INFO, FOOTER_LOG} from "../../config";
+import {EMBED_INFO, EMOJIS, FOOTER_LOG} from "../../config";
 
 export default async function (client: BernardClient, oldChannel: TextChannel, newChannel: TextChannel) {
 
@@ -10,30 +10,34 @@ export default async function (client: BernardClient, oldChannel: TextChannel, n
 
     if (oldChannel.rawPosition !== newChannel.rawPosition) return;
 
+    let channelEmoji = client.getEmoji(EMOJIS.channel),
+        categoryEmoji = client.getEmoji(EMOJIS.category),
+        channelNsfwEmoji = client.getEmoji(EMOJIS.channelNsfw);
+
     const embed = new MessageEmbed()
         .setColor(EMBED_INFO)
         .setTitle('Mise à jour Salon')
-        .addFields({name: `🎙 Name (ID)`, value: `<#${oldChannel.id}> (${oldChannel.id})`, inline: true})
+        .addFields({name: `${channelEmoji} Name (ID)`, value: `<#${oldChannel.id}> (${oldChannel.id})`, inline: true})
         .setTimestamp()
         .setFooter({text: FOOTER_LOG, iconURL: oldChannel.client.user?.displayAvatarURL({dynamic: true})});
 
     if (oldChannel.name !== newChannel.name) embed.addFields(
-        {name: `🎙 Old Name`, value: `\`${oldChannel.name}\``, inline: true},
-        {name: `🎙 Old Name`, value: `\`${newChannel.name}\``, inline: true})
+        {name: `${channelEmoji} Old Name`, value: `\`${oldChannel.name}\``, inline: true},
+        {name: `${channelEmoji} Old Name`, value: `\`${newChannel.name}\``, inline: true})
 
 
     if (oldChannel.parentId !== newChannel.parentId) embed.addFields(
-        {name: `🎙 Old category`, value: `${oldChannel.parentId ? `<#${oldChannel.parentId}>` : '`None category`'}`, inline: true},
-        {name: `🎙 New category`, value: `${newChannel.parentId ? `<#${newChannel.parentId}>` : '`None category`'}`, inline: true}
+        {name: `${categoryEmoji} Old category`, value: `${oldChannel.parentId ? `<#${oldChannel.parentId}>` : '`None category`'}`, inline: true},
+        {name: `${categoryEmoji} New category`, value: `${newChannel.parentId ? `<#${newChannel.parentId}>` : '`None category`'}`, inline: true}
     )
 
     if (oldChannel.topic !== newChannel.topic) embed.addFields(
-        {name: `🎙 Old topic`, value: `${oldChannel.topic ? oldChannel.topic : '`None topic`'}`, inline: false},
-        {name: `🎙 New topic`, value: `${newChannel.topic ? newChannel.topic : '`None topic`'}`, inline: false}
+        {name: `${channelEmoji} Old topic`, value: `${oldChannel.topic ? oldChannel.topic : '`None topic`'}`, inline: false},
+        {name: `${channelEmoji} New topic`, value: `${newChannel.topic ? newChannel.topic : '`None topic`'}`, inline: false}
     )
 
     if (oldChannel.nsfw !== newChannel.nsfw) embed.addFields(
-        {name: `🎙 Salon NSFW`, value: newChannel.nsfw ? '`Activated`' : '`Deactivated`', inline: true}
+        {name: `${channelNsfwEmoji} Channel NSFW`, value: newChannel.nsfw ? '`Activated`' : '`Deactivated`', inline: true}
     )
 
     return client.getChannel(oldChannel.guild, server, {embeds: [embed]});
