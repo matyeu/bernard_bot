74fb0f5338c4b7452325370077d183e6ce21507b
matyeu <matyeu.lts@gmail.com>
2022-06-12 21:12:00 +0200

    Add command lock

--- README.md
+++ README.md
@@ -44,8 +44,10 @@
 | Name          | Description                      | Sub-commands     | Usage                    | Cd     |
 | ------------- | -------------------------------- | ---------------- | ------------------------ | ------ |
 | ban           | Ban or unban a user.             |none              | <@user> \<reason> (time) | 1sec   |
-| kick          | Kick a user.                     |none              | <@user> \<reason>        | 1sec   |
+| Lock          | Lock or unlock a channel.        |none              | <\state>                 | 1sec   |
 | mute          | Mute or unmute a user.           |none              | <@user> \<reason> (time) | 1sec   |
+| kick          | Kick a user.                     |none              | <@user> \<reason>        | 1sec   |
+
 
 
 ## Configuration
---
+++ src/Commands/Moderation/lock.ts
@@ -0,0 +1,52 @@
+import {BernardClient} from "../../Librairie";
+import {CommandInteraction, TextChannel} from "discord.js";
+import {EMOJIS} from "../../config";
+
+export default async function (client: BernardClient, interaction: CommandInteraction) {
+
+    let channelUnlock = client.getEmoji(EMOJIS.channel),
+        channelLock = client.getEmoji(EMOJIS.channelLock);
+
+    let channel = <TextChannel>interaction.channel!;
+    let permissions = [...channel.permissionOverwrites.cache.values()];
+    let state = interaction.options.getNumber("state");
+    let everyone = channel.guild.roles.everyone;
+
+    if (state === 1) {
+        await channel.edit({
+            permissionOverwrites: [...permissions, {id: everyone.id, deny: ["SEND_MESSAGES", "VIEW_CHANNEL"]}]
+        });
+
+        await interaction.replySuccessMessage(client,`The channel is being **locked**.`, true)
+        return interaction.channel?.send({content: `${channelLock} The channel is now **locked**.`});
+
+    } else {
+        await channel.edit({
+            permissionOverwrites: [...permissions, {id: everyone.id, allow: ["SEND_MESSAGES"], deny: ["VIEW_CHANNEL"]}]
+        });
+
+        await interaction.replySuccessMessage(client,`The channel is being **unlocked**.`, true)
+        return interaction.channel?.send({content: `${channelUnlock} The channel is now **locked**.`});
+    }
+
+};
+
+export const slash = {
+    data: {
+        name: "lock",
+        description: "Lock or unlock a channel.",
+        category: "Moderation",
+        options: [
+            {
+                name: "state",
+                type: "NUMBER",
+                description: "Indicates the new state of the channel",
+                required: true,
+                choices: [
+                    {name: "Lock", value: 1},
+                    {name: "Unlock", value: 0}
+                ],
+            }],
+        defaultPermission: false,
+    }
+}
