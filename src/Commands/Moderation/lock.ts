3118b2370ab1ce6e54b526583afa1bbf802a2508
matyeu <matyeu.lts@gmail.com>
2022-06-12 21:32:20 +0200

    Add permissions lock

--- src/Commands/Moderation/lock.ts
+++ src/Commands/Moderation/lock.ts
@@ -8,22 +8,17 @@ export default async function (client: BernardClient, interaction: CommandIntera
         channelLock = client.getEmoji(EMOJIS.channelLock);
 
     let channel = <TextChannel>interaction.channel!;
-    let permissions = [...channel.permissionOverwrites.cache.values()];
     let state = interaction.options.getNumber("state");
     let everyone = channel.guild.roles.everyone;
 
     if (state === 1) {
-        await channel.edit({
-            permissionOverwrites: [...permissions, {id: everyone.id, deny: ["SEND_MESSAGES", "VIEW_CHANNEL"]}]
-        });
+        await channel.permissionOverwrites.edit(everyone.id, {SEND_MESSAGES: false})
 
         await interaction.replySuccessMessage(client,`The channel is being **locked**.`, true)
         return interaction.channel?.send({content: `${channelLock} The channel is now **locked**.`});
 
     } else {
-        await channel.edit({
-            permissionOverwrites: [...permissions, {id: everyone.id, allow: ["SEND_MESSAGES"], deny: ["VIEW_CHANNEL"]}]
-        });
+        await channel.permissionOverwrites.edit(everyone.id, {SEND_MESSAGES: null})
 
         await interaction.replySuccessMessage(client,`The channel is being **unlocked**.`, true)
         return interaction.channel?.send({content: `${channelUnlock} The channel is now **locked**.`});
@@ -37,6 +32,7 @@ export const slash = {
         description: "Lock or unlock a channel.",
         category: "Moderation",
         cooldown: 1,
+        permissions: ["MANAGE_MESSAGES"],
         options: [
             {
                 name: "state",
