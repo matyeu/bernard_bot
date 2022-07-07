d601765cfa09cd7ad3bc7e487553a09f9469926d
matyeu <matyeu.lts@gmail.com>
2022-07-07 19:32:08 +0200

    Update structure web

--- src/Librairie/dashboard.ts
+++ src/Librairie/dashboard.ts
@@ -1,5 +1,5 @@
 const express = require("express");
-const dashboard = express();
+const web = express();
 const path = require("path");
 const passport = require("passport");
 const Strategy = require("passport-discord").Strategy;
@@ -10,11 +10,11 @@ const Logger = require("../Librairie/logger");
 
 module.exports = (client: any) => {
 
-    const dashboardDirectory = path.resolve(`${process.cwd()}${path.sep}dashboard`)
+    const webDirectory = path.resolve(`${process.cwd()}${path.sep}web`)
 
-    const templatesDirectory = path.resolve(`${dashboardDirectory}${path.sep}templates`)
+    const templatesDirectory = path.resolve(`${webDirectory}${path.sep}templates`)
 
-    dashboard.use("/public", express.static(path.resolve(`${dashboardDirectory}${path.sep}public`)))
+    web.use("/public", express.static(path.resolve(`${webDirectory}${path.sep}public`)))
 
     passport.serializeUser((user: any, done: any) => {
         done(null, user);
@@ -36,18 +36,18 @@ module.exports = (client: any) => {
         }
     ))
 
-    dashboard.use(session({
+    web.use(session({
         store: new MemoryStore({ checkPeriode: 99999999 }),
         secret: process.env.SSECRET,
         resave: false,
         saveUninitialized: false,
     }))
 
-    dashboard.use(passport.initialize());
-    dashboard.use(passport.session());
+    web.use(passport.initialize());
+    web.use(passport.session());
 
-    dashboard.engine("html", require("ejs").renderFile)
-    dashboard.set("view engine", "html")
+    web.engine("html", require("ejs").renderFile)
+    web.set("view engine", "html")
 
     const renderTemplate = (res: any, req: any, template: any, data = {}) =>  {
         const baseData = {
@@ -61,22 +61,22 @@ module.exports = (client: any) => {
         )
     }
 
-    dashboard.get("/login", (req: any, res: any, next: any) => {
+    web.get("/login", (req: any, res: any, next: any) => {
             req.session.backURL = "/"
             next();
         },
         passport.authenticate("discord"));
 
-    dashboard.get("/callback", passport.authenticate("discord"), (req: any, res: any) => {
+    web.get("/callback", passport.authenticate("discord"), (req: any, res: any) => {
         res.redirect("/");
-        Logger.dashboard("User login on the dashboard")
+        Logger.dashboard("User login on the web")
     });
 
-    dashboard.get("/", (req: any, res: any) => {
+    web.get("/", (req: any, res: any) => {
         const members = client.users.cache.size
         const channels = client.channels.cache.size
         const guilds = client.guilds.cache.size
-        renderTemplate(res, req, "home.ejs", {
+        renderTemplate(res, req, "vitrine/home.ejs", {
             stats: {
                 serveurs: guilds,
                 utilisateurs : members,
@@ -85,15 +85,15 @@ module.exports = (client: any) => {
         })
     })
 
-    dashboard.get('/logout', (req: any, res: any, next: any) => {
+    web.get('/logout', (req: any, res: any, next: any) => {
         req.logout(function(err: any) {
             if (err) { return next(err); }
             res.redirect('/');
-            Logger.dashboard("User logout on the dashboard")
+            Logger.dashboard("User logout on the web")
         });
     });
 
-    client.site = dashboard.listen(process.env.PORT);
+    client.site = web.listen(process.env.PORT);
     Logger.client(`Dashboard on: http://${process.env.DOMAIN}:${process.env.PORT}`)
 
 
