0d136c53c990ec4351257481659095af3475ae10
matyeu <matyeu.lts@gmail.com>
2022-07-12 21:19:40 +0200

    Update of module Interactions in multilingue

--- src/Interactions/Buttons/Morpion/Boxs/exitMorpion-button.ts
+++ src/Interactions/Buttons/Morpion/Boxs/exitMorpion-button.ts
@@ -1,26 +1,22 @@
 import {BernardClient} from "../../../../Librairie";
 import {ButtonInteraction, MessageEmbed} from "discord.js";
 import {find} from "../../../../Models/morpion";
-import {EMBED_SUCCESS, EMOJIS} from "../../../../config";
+import {EMBED_SUCCESS} from "../../../../config";
 
 export default async function (client: BernardClient, interaction: ButtonInteraction, language: any) {
 
     let interactionTcheck = interaction.customId;
     let interactionUserId = interaction.user.id;
 
-    let error = client.getEmoji(EMOJIS.error),
-        check = client.getEmoji(EMOJIS.check);
-
     if (interactionTcheck.substring(30, 12) !== interactionUserId && interactionTcheck.split('-')[1] !== interactionUserId)
         return interaction.replyErrorMessage(client, language("ERROR_GAME"), true);
-
 
     let requestGameUser = interactionTcheck.substring(30, 12)
     let member = await interaction.guild!.members.fetch(requestGameUser)
     let morpionConfig: any = await find(interaction.guild!.id, member.user.id);
 
     if (morpionConfig.userID_1 !== interactionUserId)
-        return interaction.reply({content: `${error} | **You can** only leave **your own games**!`, ephemeral: true});
+        return interaction.replyErrorMessage(client, language("ERROR_LEAVE"), true);
 
     await morpionConfig.delete();
 
--- src/Librairie/languages/fr-FR/Interactions/Buttons/Morpion/boxsButtonData.ts
+++ src/Librairie/languages/fr-FR/Interactions/Buttons/Morpion/boxsButtonData.ts
@@ -5,8 +5,8 @@ const boxsButtonButtonFR = {
     CONTENT_VICTORY: "❗️ | %memberWin% **a gagné** la partie contre %memberLost%.",
     CONTENT_BETWEEN: "❗️ | La partie entre %member1% et %member2% st terminée : **Match NUL**",
     CONTENT_GIVING_UP: "**%member1% vs %member2%\n❗️ %memberWin% **has won the game** as %memberLost% **is giving up the game**.",
-    ERROR_LEAVE: "**You can** only leave **your own games**!",
-    GAME_CANCEL: "%member% your game has been **cancelled**!",
+    ERROR_LEAVE: "**Vous ne pouvez** quitter que **vos propres parties** !",
+    GAME_CANCEL: "%member% **votre** partie a bien été **annulée** !",
 };
 
 
